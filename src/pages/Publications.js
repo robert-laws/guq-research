534ba56503eeda408da35e400e7e1f9d6b34ae55
Robert Laws <roblaws@me.com>
2023-07-11 16:53:26 +0300

    add bubbles for filter selections

--- src/pages/Publications.js
+++ src/pages/Publications.js
@@ -42,6 +42,8 @@ export const Publications = () => {
 
   const [currentPage, setCurrentPage] = useState(1);
   const [publicationsPerPage, setPublicationsPerPage] = useState(10);
+  const [buttonFilterSpinner, setButtonFilterSpinner] = useState(false);
+  const [selectedFiltersArray, setSelectedFiltersArray] = useState([]);
 
   // initializes the publications list
   useEffect(() => {
@@ -222,6 +224,26 @@ export const Publications = () => {
     }
   };
 
+  // const updateFiltersFromBubble = (filterItem) => {
+  //   setFiltersTouched(true);
+  //   const keys = Object.keys(filters);
+
+  //   keys.forEach((key) => {
+  //     if (filters[key].includes(filterItem)) {
+  //       const newFilters = filters[key].filter((item) => item !== filterItem);
+
+  //       setFilters({
+  //         ...filters,
+  //         [key]: newFilters,
+  //       });
+  //     }
+  //   });
+
+  //   setSelectedFiltersArray(
+  //     selectedFiltersArray.filter((item) => item !== filterItem)
+  //   );
+  // };
+
   // used to build the checkbox inputs for each filter list on page load
   const getLists = (lists) => {
     const allLists = [];
@@ -271,27 +293,48 @@ export const Publications = () => {
 
   // code used to apply filters to the publications list when a checkbox is clicked
   useEffect(() => {
+    // const filtersArray = Object.entries(filters);
+    // const applyFilters = (filterArray) => {
+    //   let filteredPublications = [];
+    //   searchResults.length > 0
+    //     ? (filteredPublications = searchResults)
+    //     : (filteredPublications = publications);
+    //   for (let i = 0; i < filterArray.length; i++) {
+    //     const list = filterArray[i][0];
+    //     const filters = filterArray[i][1];
+    //     if (filters.length > 0) {
+    //       filteredPublications = filteredPublications.filter((publication) =>
+    //         filters.includes(publication[list])
+    //       );
+    //     }
+    //   }
+    //   return filteredPublications;
+    // };
+    // if (filtersTouched) {
+    //   filterPublications(applyFilters(filtersArray));
+    // }
+    // const allFiltersList = getAllSelectedFiltersArray();
+    // setSelectedFiltersArray(allFiltersList);
+  }, [filters]);
+
+  const getAllSelectedFiltersArray = () => {
     const filtersArray = Object.entries(filters);
-    const applyFilters = (filterArray) => {
-      let filteredPublications = [];
-      searchResults.length > 0
-        ? (filteredPublications = searchResults)
-        : (filteredPublications = publications);
-      for (let i = 0; i < filterArray.length; i++) {
-        const list = filterArray[i][0];
-        const filters = filterArray[i][1];
-        if (filters.length > 0) {
-          filteredPublications = filteredPublications.filter((publication) =>
-            filters.includes(publication[list])
-          );
+    let filteredSelections = [];
+
+    filtersArray.forEach((item) => {
+      if (item[1].length > 0) {
+        if (item[1].length === 1) {
+          filteredSelections.push(item[1]);
+        } else {
+          item[1].forEach((filter) => {
+            filteredSelections.push(filter);
+          });
         }
       }
-      return filteredPublications;
-    };
-    if (filtersTouched) {
-      filterPublications(applyFilters(filtersArray));
-    }
-  }, [filters]);
+    });
+
+    return filteredSelections;
+  };
 
   const handleListToggle = (index) => {
     const list = filterListRef.current[index];
@@ -332,6 +375,7 @@ export const Publications = () => {
     setLocalQuery('');
     setQuery('');
     setSearchResults([]);
+    setSelectedFiltersArray([]);
   };
 
   // pagination
@@ -348,6 +392,9 @@ export const Publications = () => {
 
   // filter buttons
   const handleFilterClick = () => {
+    // hide/show spinner
+    setButtonFilterSpinner(true);
+
     // moved filtering functionality from useEffect after filters to a button click
     const filtersArray = Object.entries(filters);
 
@@ -374,6 +421,12 @@ export const Publications = () => {
     if (filtersTouched) {
       filterPublications(applyFilters(filtersArray));
     }
+
+    const allFiltersList = getAllSelectedFiltersArray();
+    setSelectedFiltersArray(allFiltersList);
+
+    // hide/show spinner
+    setButtonFilterSpinner(false);
   };
 
   useEffect(() => {
@@ -440,6 +493,34 @@ export const Publications = () => {
               </div>
             </form>
           </div>
+
+          <div className='flex py-1 my-1 mx-28 rounded-lg'>
+            {selectedFiltersArray.length > 0 &&
+              selectedFiltersArray.map((filter) => {
+                // console.log(filter);
+                return (
+                  <div key={filter} className='flex justify-center'>
+                    <div className='inline-flex items-center px-3.5 py-0.5 rounded-full text-md font-medium bg-blue-100 text-gray-800 mr-1 mb-2'>
+                      {filter}
+                      {/* <svg // X icon
+                        xmlns='http://www.w3.org/2000/svg'
+                        className='h-4 w-4 ml-1'
+                        viewBox='0 0 20 20'
+                        fill='currentColor'
+                        // onClick={() => updateFiltersFromBubble(filter)}
+                      >
+                        <path
+                          fillRule='evenodd'
+                          d='M10 12.586L5.707 16.88a1 1 0 11-1.414-1.414L8.586 11 4.293 6.707a1 1 0 111.414-1.414L10 9.586l4.293-4.293a1 1 0 111.414 1.414L11.414 11l4.293 4.293a1 1 0 11-1.414 1.414L10 12.586z'
+                          clipRule='evenodd'
+                        />
+                      </svg> */}
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+
           <div className='py-6'>
             <div className='relative mx-auto flex flex-col max-w-8xl justify-center sm:px-2 lg:flex-row lg:px-2 xl:px-4'>
               <div className='flex-auto lg:relative lg:block lg:flex-none mb-6 lg:mb-0'>
@@ -452,6 +533,7 @@ export const Publications = () => {
                       <div className='flex justify-center'>
                         {/* Keyword Search Form */}
                       </div>
+
                       <div className='sm:mt-0 mt-2 mb-4'>
                         <button
                           onClick={handleResetClick}
@@ -543,15 +625,53 @@ export const Publications = () => {
                                 {list[1].length > 5 ? 'Show More' : ''}
                               </p>
                               {/* Filter Button for Each Group */}
-                              {/* <div className='flex flex-row justify-end mt-1'>
-                                <button
+                              <div className='flex flex-row justify-end mt-1'>
+                                {/* <button
                                   onClick={() => handleFilterClick()}
                                   type='button'
                                   className='rounded bg-green-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600'
                                 >
                                   Apply Filter
+                                </button> */}
+                                {/* <div className='p-1'>&nbsp;</div> */}
+                                <button
+                                  type='button'
+                                  onClick={() => handleFilterClick()}
+                                  className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-5 py-1.5 text-center ml-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center'
+                                  // style={
+                                  //   buttonFilter
+                                  //     ? { display: 'block' }
+                                  //     : { display: 'none' }
+                                  // }
+                                >
+                                  <div
+                                    style={
+                                      buttonFilterSpinner
+                                        ? { display: 'block' }
+                                        : { display: 'none' }
+                                    }
+                                  >
+                                    <svg
+                                      aria-hidden='true'
+                                      role='status'
+                                      className='inline w-4 h-4 mr-3 text-white animate-spin'
+                                      viewBox='0 0 100 101'
+                                      fill='none'
+                                      xmlns='http://www.w3.org/2000/svg'
+                                    >
+                                      <path
+                                        d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
+                                        fill='#E5E7EB'
+                                      />
+                                      <path
+                                        d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
+                                        fill='currentColor'
+                                      />
+                                    </svg>
+                                  </div>
+                                  Apply Filter
                                 </button>
-                              </div> */}
+                              </div>
                             </div>
                           );
                         })}
