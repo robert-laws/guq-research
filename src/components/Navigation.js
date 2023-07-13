a0e5d143204be5fd79d7c5f87df57fed33a22729
Robert Laws <roblaws@me.com>
2023-07-13 09:23:01 +0300

    update intro text and navigation for mobile

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -39,6 +39,7 @@ export const Navigation = () => {
               />
             </Link>
 
+            {/* desktop navigation */}
             <div className='ml-12 hidden space-x-8 lg:block'>
               <Menu
                 as='div'
@@ -300,7 +301,83 @@ export const Navigation = () => {
           </div>
         </div>
 
+        {/* mobile navigation */}
         <div className='flex flex-wrap justify-start space-x-6 py-4 lg:hidden'>
+          <Menu
+            as='div'
+            className='sm:relative inline-block text-left ml-8 mb-4'
+          >
+            <div>
+              <Menu.Button className='inline-flex w-full justify-center gap-x-1.5 rounded-md bg-transparent px-0 font-semibold text-white hover:bg-transparent'>
+                About
+                {/* <ChevronDownIcon
+                      className='-mr-1 h-5 w-5 text-gray-400'
+                      aria-hidden='true'
+                    /> */}
+              </Menu.Button>
+            </div>
+
+            <Transition
+              as={Fragment}
+              enter='transition ease-out duration-100'
+              enterFrom='transform opacity-0 scale-95'
+              enterTo='transform opacity-100 scale-100'
+              leave='transition ease-in duration-75'
+              leaveFrom='transform opacity-100 scale-100'
+              leaveTo='transform opacity-0 scale-95'
+            >
+              <Menu.Items className='absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                <div className='py-1'>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        to='/introduction'
+                        className={classNames(
+                          active
+                            ? 'bg-gray-100 text-gray-900'
+                            : 'text-gray-700',
+                          'block px-4 py-2 text-sm'
+                        )}
+                      >
+                        Introduction
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        to='/data-sources'
+                        className={classNames(
+                          active
+                            ? 'bg-gray-100 text-gray-900'
+                            : 'text-gray-700',
+                          'block px-4 py-2 text-sm'
+                        )}
+                      >
+                        Data Sources
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        to='/methodology'
+                        className={classNames(
+                          active
+                            ? 'bg-gray-100 text-gray-900'
+                            : 'text-gray-700',
+                          'block px-4 py-2 text-sm'
+                        )}
+                      >
+                        Methodology
+                      </Link>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+
           {navigation.map((link) => (
             <a
               key={link.name}
@@ -310,7 +387,10 @@ export const Navigation = () => {
               {link.name}
             </a>
           ))}
-          <Menu as='div' className='sm:relative inline-block text-left ml-8'>
+          <Menu
+            as='div'
+            className='sm:relative inline-block text-left ml-8 mb-4'
+          >
             <div>
               <Menu.Button className='inline-flex w-full justify-center gap-x-1.5 rounded-md bg-transparent px-0 font-semibold text-white hover:bg-transparent'>
                 Annual Reports
@@ -388,7 +468,10 @@ export const Navigation = () => {
             </Transition>
           </Menu>
 
-          <Menu as='div' className='sm:relative inline-block text-left ml-8'>
+          <Menu
+            as='div'
+            className='sm:relative inline-block text-left ml-8 mb-4'
+          >
             <div>
               <Menu.Button className='inline-flex w-full justify-center gap-x-1.5 rounded-md bg-transparent px-0 font-semibold text-white hover:bg-transparent'>
                 Research Impact
